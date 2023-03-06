9ff1a27f033f79034292cabc13552793fbd1b02f
Reece Pohlman <reecepohlman@gmail.com>
2023-03-06 00:19:26 -0500

    updated poorly written code and implemented random btn functionality

--- script.js
+++ script.js
@@ -1,97 +1,86 @@
 const container = document.querySelector('#container');
-const gridDiv = document.createElement('div');
-let on = true;
-//list to get ride of current grid
-let hide_current_div = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-// z is select the item in the array
-let z = 0;
+const RED = '#F65058FF';
+const YELLOW = '#FBDE44FF';
+
+let randomMode = false;
+let borderOn = true;
 window.grid_size = 16;
 
-//makes the default 16x16 Grid
-for(i = 0; i< 256; i++) {
-  const gridDiv = document.createElement('div');
-  gridDiv.classList.add('grid');
-  gridDiv.id = 'id' + hide_current_div[z] + i
-  container.appendChild(gridDiv);
-}
-// color changer as hover
-for(n = 0; n< 256; n ++) {
-  let cell = 'id' + hide_current_div[z] + n
-  document.getElementById(cell).addEventListener('mouseover', function (){
-    document.getElementById(cell).style.backgroundColor = '#F65058FF'; //color red
-  })
-}
-// function for clear button
-function clear() {
-  for(n = 0; n < window.grid_size * window.grid_size; n ++) {
-    let cell = 'id' + hide_current_div[z] + n
-    document.getElementById(cell).style.backgroundColor = '#FBDE44FF'; // color yellow
-  }
-}
-// function for new grid button
-function new_grid() {
-  let root = document.documentElement;
-  hide();
-  window.grid_size = parseInt(prompt('What Grid Size?'));
-  let grid_size = window.grid_size
-  z = z + 1;
-  root.style.setProperty('--grid_row', grid_size);
-  root.style.setProperty('--grid_col', grid_size);
-    //puts new grid on screen
-  for(t = 0; t < grid_size * grid_size ; t++) {
+// makes the default 16x16 Grid
+createGrid(16);
+
+function createGrid(gridSize){
+  for(i = 0; i< gridSize*gridSize; i++) {
     const gridDiv = document.createElement('div');
     gridDiv.classList.add('grid');
-    gridDiv.id = 'id' + hide_current_div[z] + t;
+    gridDiv.id = 'id' + i
     container.appendChild(gridDiv);
-      //adds hover feature
+    //adds hover feature
     let cell = gridDiv.id
-    document.getElementById(cell).addEventListener('mouseover', function (){
-      document.getElementById(cell).style.backgroundColor = '#F65058FF'; //color red
-    })
+    document.getElementById(cell).addEventListener('mouseover', () => {
+      (!randomMode? document.getElementById(cell).style.backgroundColor = RED : document.getElementById(cell).style.backgroundColor = randomColor());
+    });
   }
 }
-// --part of function new_grid-- function to get ride of current grid on screen
-  function hide() {
-    if (z == 0){
-      for(n = 0; n< 256; n ++) {
-      let cell = 'id' + hide_current_div[z] + n;
+
+function randomColor() {
+  let color = [];
+  for (let i = 0; i < 3; i++) {
+    color.push(Math.floor(Math.random() * 256));
+  }
+  return 'rgb(' + color.join(', ') + ')';
+} 
+
+// Part of function new_grid-- function to get ride of current grid on screen
+  function destroyGrid() {
+    for(i = 0; i< window.grid_size * window.grid_size; i++) {
+      let cell = 'id' + i;
       let cellid = document.getElementById(cell);
       cellid.remove();
-      }
-    }else {
-      for(n = 0; n< window.grid_size * window.grid_size; n ++) {
-      let cell = 'id' + hide_current_div[z] + n;
-      let cellid = document.getElementById(cell);
-      cellid.remove();
-      }
     }
   }
+
+// !---------------------Button Section--------------------------------!
+
+// function for new grid button
+function newGridBtn() {
+  destroyGrid();
+  let root = document.documentElement;
+  window.grid_size = parseInt(prompt('What Grid Size?', 16));
+  root.style.setProperty('--grid_row', window.grid_size); // using variable--grid_row in css for the grid-template-row
+  root.style.setProperty('--grid_col', window.grid_size); // using variable--grid_col in css for the grid-template-column
+  //puts new grid on screen
+  createGrid(window.grid_size);
+}
+
 // function to either remove or add the border
-function border(){
-  if (on == false){
-    for(n = 0; n< window.grid_size * window.grid_size; n ++) {
-      let cell = 'id' + hide_current_div[z] + n;
-      let cellid = document.getElementById(cell);
-      cellid.style.border = '1px solid gray';
-      on = true;
-    }
-  }else{
-    for(n = 0; n< window.grid_size * window.grid_size; n ++) {
-      let cell = 'id' + hide_current_div[z] + n;
-      let cellid = document.getElementById(cell);
-      cellid.style.border = '0';
-      on = false;
-    }
+function borderBtn(){
+  (borderOn ? borderOn = false : borderOn = true);
+  for(n = 0; n< window.grid_size * window.grid_size; n ++) {
+    let cell = 'id' + n;
+    let cellid = document.getElementById(cell);
+    (borderOn ? cellid.style.border = '1px solid gray' :  cellid.style.border = '0');
   }
 }
+
+// function for clear button
+function clearBtn() {
+  for(i = 0; i < window.grid_size * window.grid_size; i++) {
+    let cell = 'id' + i
+    document.getElementById(cell).style.backgroundColor = YELLOW;
+  }
+}
+
+function randomBrushBtn(){
+  randomMode = true;
+}
+function redBrushBtn(){
+  randomMode = false;
+}
+
 // event listeners for buttons
-document.getElementById('new_grid').addEventListener("click", new_grid);
-document.getElementById('clear').addEventListener("click", clear);
-document.getElementById('border').addEventListener('click', border);
-
-
-
-// okay the hide() function will run update to the size of the previous grid but then ERRORs,
-// example: if i make new grid 4 it will work but if i try to change it again it will run/remove uptill it gets to 4*4
-// so i need to make it so that it removes the specfic prvious vaule, not the new grid number
-//--------------------------------need to fix the hide() function!!-----------------------------------------------------
+document.getElementById('new_grid').addEventListener("click", newGridBtn);
+document.getElementById('clear').addEventListener("click", clearBtn);
+document.getElementById('border').addEventListener('click', borderBtn);
+document.getElementById('random_brush').addEventListener('click', randomBrushBtn);
+document.getElementById('red_brush').addEventListener('click', redBrushBtn);
